2b1487e24b715114cc378da4a9c7912e59bd73af
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2023-05-01 20:25:12 +0200

    Add fahrenheit toggle

--- src/script.js
+++ src/script.js
@@ -5,12 +5,18 @@
 const searchBox = document.querySelector("#searchbox");
 const degreesDisplay = document.querySelector("#degrees");
 const errorMessageDisplay = document.querySelector("#errorMessage");
+const feelsLike = document.querySelector("#feelsLike");
+const alternativeUnit = document.querySelector("#alternativeUnit");
 
 searchBox.addEventListener("keypress", (e) => {
   if (e.keyCode === 13) {
     getWetherData(searchBox.value);
   }
 });
+alternativeUnit.addEventListener("click", () => {
+  getWetherData(searchBox.value);
+});
+
 getWetherData("älmhult");
 
 async function getWetherData(location) {
@@ -20,11 +26,17 @@ async function getWetherData(location) {
       `https://api.weatherapi.com/v1/current.json?key=9f98e1ed40d24c118f4192729232004&q=${location}`
     );
     let json = await request.json();
-    console.log(json.current.temp_c);
+    //console.log(json.current);
 
-    degreesDisplay.innerHTML = json.current.temp_c + "°C";
+    if (!alternativeUnit.checked) {
+      degreesDisplay.innerHTML = json.current.temp_c + "°C";
+      feelsLike.textContent = `feels like ${json.current.feelslike_c}°C`;
+    } else {
+      degreesDisplay.innerHTML = json.current.temp_f + "°F";
+      feelsLike.textContent = `feels like ${json.current.feelslike_f}°F`;
+    }
   } catch (error) {
-    errorMessageDisplay.textContent = `please enter a valid locaton, ${location} is not valid`;
+    errorMessageDisplay.textContent = `please enter a valid locaton, "${location}" is not valid`;
     errorMessageDisplay.classList.remove("hidden");
   }
 }
